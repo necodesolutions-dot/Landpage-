5ba9b363dba899115e9fb9b736daf8c18e12ed99
Pedro  Emmanuel G. C. Machado <132278032+Pedro-Emmanuel-G-C-Machado@users.noreply.github.com>
2025-10-31 16:37:56 -0300

    Começo da estilização e criação do componente do footer

--- landpage/src/App.jsx
+++ landpage/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Header } from './componets/header/Header'
-
+import "./global.css"
 export function App() {
 
   return (
     <>
   <Header/>
+  <div className="app">
+
+  </div>
     </>
   )
 }
---
+++ landpage/src/pages/home/time/Time.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export function Time() {
+  return (
+    <div>Time</div>
+  )
+}
+
